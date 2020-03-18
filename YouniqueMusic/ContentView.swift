224d29677546126c968cffd8388c6c1fe28a4235
Yining Liu <yliu327@syr.edu>
2020-03-18 19:13:12 -0400

    Initial Commit

---
+++ YouniqueMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YouniqueMusic
+//
+//  Created by WInnie Tam on 3/18/20.
+//  Copyright © 2020 Yining Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
